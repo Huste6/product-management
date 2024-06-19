c208812c12f84289416f9f8dd016c78b579b3109
Huste6 <quannghihai1@gmail.com>
2024-06-19 09:30:06 +0700

    toi uu phan tim kiem admin

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,5 +1,6 @@
 const Product = require("../../models/product.model");
 const filterStatusHelper=require("../../helpers/filterStatus")
+const SearchHelper = require("../../helpers/search")
 
 // [GET] /admin/product
 module.exports.index = async (req, res) => {
@@ -10,19 +11,13 @@ module.exports.index = async (req, res) => {
         let find = {
             deleted: false
         };
-        let keyword=""
-
         if (req.query.status) {
             find.status = req.query.status;
         }
-
-        if (req.query.keyword) {
-            keyword = req.query.keyword
-
-            //c1: find.title = { $regex: keyword, $options: "i" };
-            // c2:
-            const regex = new RegExp(keyword,"i")
-            find.title=regex
+        // Doan code phan tim kiem
+        const ObjectSearch = SearchHelper(req.query);
+        if(ObjectSearch.regex){
+            find.title=ObjectSearch.regex
         }
 
         const products = await Product.find(find);
@@ -31,7 +26,7 @@ module.exports.index = async (req, res) => {
             pageTitle: "Danh sach san pham",
             products: products,
             filterStatus: filterStatus,
-            keyword: keyword
+            keyword: ObjectSearch.keyword
         });
     } catch (error) {
         console.error(error);
