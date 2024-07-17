1ca404f5482208f18b08d487243c16b9d09041f3
Huste6 <quannghihai1@gmail.com>
2024-07-17 10:38:16 +0700

    update upload anh

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -146,17 +146,19 @@ if(uploadImage){
     const uploadImageInput = document.querySelector("[upload-image-input]")
     const uploadImagePreview = document.querySelector("[upload-image-preview]");
     const closeButton = document.querySelector(".close");
-
+    const imageContainer = document.querySelector('.image-container');
     uploadImageInput.addEventListener("change",(e)=>{
         const file = e.target.files[0]
         if(file){
             uploadImagePreview.src = URL.createObjectURL(file);
+            imageContainer.style.display = 'inline-block';
         }
     })
 
     closeButton.addEventListener("click",()=>{
         uploadImageInput.value='';
         uploadImagePreview.src='';
+        imageContainer.style.display = 'none';
     })
 }
 // end upload image
