5bb8272e178d0e5154e64fd99652bf6a7285dec3
Huste6 <quannghihai1@gmail.com>
2024-07-17 15:03:11 +0700

    validate du lieu

--- routes/admin/product.router.js
+++ routes/admin/product.router.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const storageMulter = require("../../helpers/storageMulter")
 const upload = multer({ storage: storageMulter() })
 const controller = require("../../controllers/admin/product.controller")
+const validate = require("../../validate/admin/product.validate")
 
 router.get('/', controller.index);
 
@@ -15,6 +16,11 @@ router.delete('/delete/:id', controller.DeleteItem);
 
 router.get('/create', controller.create); // lay ra trang tao moi san pham
 
-router.post('/create',upload.single('thumbnail'),controller.createPost);
+router.post(
+    '/create',
+    upload.single('thumbnail'),
+    validate.createPost,
+    controller.createPost
+);
 
 module.exports = router;
