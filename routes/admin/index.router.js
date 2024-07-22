6a9d6da60f74502d4bfc877783f4df8f030b7f9f
Huste6 <quannghihai1@gmail.com>
2024-07-22 19:23:55 +0700

    thêm middleware cho router private

--- routes/admin/index.router.js
+++ routes/admin/index.router.js
@@ -1,4 +1,5 @@
 const systemConfig = require('../../config/system')
+const authmiddleware = require('../../middlewares/admin/auth.middleware')
 const dashboardRouter = require('./dashboard.router')
 const productRouter = require('./product.router')
 const productCategoryRouter = require('./product-category.router')
@@ -6,13 +7,13 @@ const RoleRouter = require("./role.router")
 const AccountRouter = require("./account.router")
 const AuthRouter = require("./auth.router")
 
-module.exports=(app)=>{
+module.exports = (app) => {
     const PATH_ADMIN = systemConfig.prefixAdmin
-    
-    app.use(PATH_ADMIN+'/dashboard',dashboardRouter);
-    app.use(PATH_ADMIN+'/products',productRouter)
-    app.use(PATH_ADMIN+'/product-category',productCategoryRouter)
-    app.use(PATH_ADMIN+'/roles',RoleRouter)
-    app.use(PATH_ADMIN+'/accounts',AccountRouter)
-    app.use(PATH_ADMIN+'/auth',AuthRouter)
+
+    app.use(PATH_ADMIN + '/dashboard', authmiddleware.requireAuth, dashboardRouter);
+    app.use(PATH_ADMIN + '/products', authmiddleware.requireAuth, productRouter)
+    app.use(PATH_ADMIN + '/product-category', authmiddleware.requireAuth, productCategoryRouter)
+    app.use(PATH_ADMIN + '/roles', authmiddleware.requireAuth, RoleRouter)
+    app.use(PATH_ADMIN + '/accounts', authmiddleware.requireAuth, AccountRouter)
+    app.use(PATH_ADMIN + '/auth', AuthRouter)
 }
