e4211f1930c12bd49c92c44583fb591e95634ad2
Huste6 <quannghihai1@gmail.com>
2024-07-24 19:36:04 +0700

    trang tìm kiếm bên client

--- routes/client/index.router.js
+++ routes/client/index.router.js
@@ -1,8 +1,10 @@
 const productRouter = require("./product.route")
 const homeRouter = require('./home.router')
 const categoryMiddleware = require('../../middlewares/client/product-category.middleware')
+const searchRouter = require("./search.router");
 module.exports = (app) => {
     app.use(categoryMiddleware.category)
     app.use('/', homeRouter);
     app.use("/product", productRouter);
+    app.use("/search", searchRouter);
 }
