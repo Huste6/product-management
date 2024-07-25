9e768bb0535c4a6b8b39277b3fc9089bb36587f7
Huste6 <quannghihai1@gmail.com>
2024-07-25 14:53:25 +0700

    Tạo collection và model trang đặt hànghàng

---
+++ models/order.model.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose")
+
+const OrderSchema = new mongoose.Schema(
+    {
+        // user_id: String,
+        cart_id: String,
+        userInfo:{
+            fullName: String,
+            phone: String,
+            address: String
+        },
+        products:[
+            {
+                product_id: String,
+                quantity: Number,
+                discountPercent: Number,
+                price: Number
+            }
+        ],
+        deleted: {
+            type: Boolean,
+            default: false,
+        },
+        deletedAt: Date,
+    },
+    {
+        timestamps:true
+    }
+)
+
+const Order = mongoose.model("Order", cartSchema, "orders");
+module.exports = Order;
